1834ae5a9f30d6badb8bd8a6b721a82f41f133a9
samed-tech <sd0713265@gmail.com>
2026-08-03 13:09:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("AutoCar başlatıldı.");
-
-    const loginButton = document.querySelector(".login");
-    const registerButton = document.querySelector(".register");
-
-    if (loginButton) {
-        loginButton.onclick = () => {
-            alert("Giriş sistemi yakında eklenecek.");
-        };
-    }
-
-    if (registerButton) {
-        registerButton.onclick = () => {
-            alert("Kayıt sistemi yakında eklenecek.");
-        };
-    }
+document.querySelectorAll(".card button").forEach(button => {
+  button.addEventListener("click", () => {
+    button.innerText = "❤️ Favorilere Eklendi";
+    button.style.background = "#ff3b30";
+  });
 });
